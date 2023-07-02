7dc230bc2239877a2f2de1d66b03f1bcc1a7b364
jamie-codez <cruiseomondi90@gmail.com>
2023-07-02 17:29:04 +0300

    Configuring pydantic

--- app/server/app.py
+++ app/server/app.py
@@ -3,6 +3,6 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/", tags=["Root route"],response_description="Returns greeting message")
+@app.get("/", tags=["Root route"], response_description="Returns greeting message")
 async def read_root():
     return {"message": "Welcome to Student crud Ops"}
